26f870dffbaf729de85fc6b347df130680179e95
ryskiz <ryan.skinner1193@gmail.com>
2016-01-28 18:44:12 -0500

    Trying to get scroll animation working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-angular.module("app", [])
+angular.module("app", ['angular-scroll-animate', 'ngAnimate'])
+.controller('exampleController', function($scope){
+    $scope.examples = [
+    {
+    	name: "Ryan",
+	 	description: "programming dude"},
+	{
+		name: "Brea",
+	 	description: "programming dudette"}
+    ];
+
+    $scope.animateElementIn = function($el) {
+    $el.removeClass('hidden');
+    $el.addClass('animated fadeInUp'); // this example leverages animate.css classes
+  };
+
+  $scope.animateElementOut = function($el) {
+    $el.addClass('hidden');
+    $el.removeClass('animated fadeInUp'); // this example leverages animate.css classes
+  };
+});
